b66e1ad627ff7a52ad63545c793c181c3e5c0e00
NamanMalhotra2912 <neerajmalhotra0001@gmail.com>
2021-03-07 21:56:48 +0530

    Welcome to Data Structure using Hash Table

---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package HashTableApi;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Data Structure problem using Hash Table");
+	}
+
+}
